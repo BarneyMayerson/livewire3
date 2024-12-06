a9c902bf5fd5568c69ed09461038814d7ab2dafd
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-12-06 10:52:39 +0300

    09 Using Events

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,7 +11,11 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<body 
+    x-data 
+    x-on:click="$dispatch('search:clear-results')"
+    class="font-sans antialiased dark:bg-black dark:text-white/50" 
+>
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div class="relative flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -4,17 +4,9 @@
             <input
                 wire:model.live.debounce="searchText"
                 type="text" 
-                class="bg-gray-700 w-3/4 rounded p-4 text-white" 
+                class="bg-gray-700 w-full rounded p-4 text-white" 
                 placeholder="{{ $placeholder }}"
             >
-            <button
-                wire:click.prevent="clear()"
-                type="button"
-                {{ empty($searchText) ? 'disabled' : '' }} 
-                class="font-medium text-white bg-blue-600 rounded-md p-4 disabled:bg-blue-400"
-            >
-                Clear
-            </button>
         </div>
     </form>
     <livewire:search-results :results="$results" :show="!empty($searchText)">  

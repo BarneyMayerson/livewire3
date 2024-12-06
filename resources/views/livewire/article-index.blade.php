02f4bffda9a90d0b397acf9d7218ae1f28af9610
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-12-06 14:54:41 +0300

    11 Iterating Over Collections

---
+++ resources/views/livewire/article-index.blade.php
@@ -0,0 +1,14 @@
+<div class="mx-auto w-2/3">
+    @foreach ($articles as $article)
+    <div class="mt-5 p-3" wire:key="{{ $article->id }}">
+        <h3 class="text-2xl text-blue-500 mb-3 hover:text-blue-700">
+            <a href="/articles/{{$article->id}}" class="">
+                {{ $article->title }}
+            </a> 
+        </h3>
+        <p>
+        {{ str($article->content)->words(40) }}
+        </p>
+    </div>
+    @endforeach
+</div>
--- resources/views/livewire/show-article.blade.php
+++ resources/views/livewire/show-article.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="mx-auto w-2/3">
     <h2 class="text-2xl text-white">{{$article->title}}</h2>
     <div class="mt-4">
-        {{$article->content}}
+        {{ $article->content }}
     </div>
 </div>

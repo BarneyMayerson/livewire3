ace93c462e141e95edb0910b853dd33e63ea79b9
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-12-14 16:12:42 +0300

    29 Handling Authentication

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,25 @@ use App\Livewire\ArticleList;
 use App\Livewire\CreateArticle;
 use App\Livewire\Dashboard;
 use App\Livewire\EditArticle;
+use App\Livewire\Login;
 use App\Livewire\ShowArticle;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', ArticleIndex::class);
-Route::get('/dashboard', Dashboard::class);
-Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
-Route::get('/dashboard/articles/create', CreateArticle::class);
-Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
-
+Route::get('/', ArticleIndex::class)->name('home');
 Route::get('/articles/{article}', ShowArticle::class);
+
+Route::get('/login', Login::class)->name('login');
+Route::get('/logout', function () {
+    Auth::logout();
+
+    return to_route('home');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', Dashboard::class)->name('dashboard');
+    Route::get('/dashboard/articles', ArticleList::class)->name('dashboard.articles.index');
+    Route::get('/dashboard/articles/create', CreateArticle::class);
+    Route::get('/dashboard/articles/{article}/edit', EditArticle::class);
+
+});
